7644d301f6dfb2d0e81c3cc5154c795ff9de27e1
vishhaldawane <vishalviniing@gmail.com>
2024-10-09 09:57:48 +0530

    Curd1Curd2Curd3

---
+++ AssociationProject2/src/PassingObjectTest.java
@@ -0,0 +1,282 @@
+//lEARNing Frequency = Saraswati - Goddess of Wisdom (applied knowledge)
+
+//transitory - Person, Artist, GraphicDesigner, Magician
+//stationary - Account, Pen, SketchPen, Table
+
+public class PassingObjectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Lassi lassi = new Lassi("Thick",8,250,5.0f);
+		
+		//System.out.println("lassi is "+lassi);
+		
+		Curd curd1 = new Curd("Thick","Sour","Buffalow",500);
+		Curd curd2 = curd1;
+		
+		System.out.println("curd1  : "+curd1.hashCode());
+		System.out.println("curd2  : "+curd2.hashCode());
+		
+		System.out.println("curd1 "+curd1);
+		
+		System.out.println("changing the quantity...");
+		curd2.setQuantity(600);
+		System.out.println("quantity changed....");
+		
+		System.out.println("curd1 "+curd1);
+		System.out.println("curd2 "+curd2);
+		
+		
+		Curd curd3 = curd2;
+		
+		System.out.println("changing the thickness...");
+		curd3.setDensity("thin");
+		System.out.println("thickness changed....");
+		
+		System.out.println("curd1 "+curd1);
+		System.out.println("curd2 "+curd2);
+		System.out.println("curd3 "+curd3);
+		
+		
+	}
+
+}
+
+
+
+
+
+
+/*
+ * 
+ * 
+ * 	Sugar sugar = new Sugar("Organic","Brown",100);
+		Ice ice = new Ice("Bisleri","Cube",15);
+		
+		Chef chef = new Chef();
+		chef.cook(curd, sugar, ice);
+ */
+class Person
+{
+	
+}
+class Chef extends Person //isA
+{
+	
+	void cook(Curd curd, Sugar sugar, Ice ice)
+	{
+		System.out.println("Chef is cooking something...");
+		System.out.println("Chef is using Curd of "+curd.getSource()+" milk");
+	}
+}
+
+
+class Sugar
+{
+	private String type;
+	private String color;
+	private int quantity;
+	public Sugar(String type, String color, int quantity) {
+		super();
+		this.type = type;
+		this.color = color;
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "Sugar [type=" + type + ", color=" + color + ", quantity=" + quantity + "]";
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
+
+class Ice
+{
+	private String source;
+	private String shape;
+	private int quantity;
+	
+	
+	
+	public Ice(String source, String shape, int quantity) {
+		super();
+		this.source = source;
+		this.shape = shape;
+		this.quantity = quantity;
+	}
+
+
+
+	public String getSource() {
+		return source;
+	}
+
+
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+
+
+	public String getShape() {
+		return shape;
+	}
+
+
+
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Ice [source=" + source + ", shape=" + shape + ", quantity=" + quantity + "]";
+	} 
+	
+	
+}
+class Curd
+{
+	private String density;
+	private String taste;
+	private String source;
+	private int quantity;
+	
+	public Curd(String density, String taste, String source, int quantity) {
+		super();
+		this.density = density;
+		this.taste = taste;
+		this.source = source;
+		this.quantity = quantity;
+	}
+
+	public String getDensity() {
+		return density;
+	}
+
+	public void setDensity(String density) {
+		this.density = density;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Curd [density=" + density + ", taste=" + taste + ", source=" + source + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+	
+}
+class Lassi
+{
+	String density;
+	int sweetLevel;
+	int quantity;
+	float temperature;
+	public Lassi(String density, int sweetLevel, int quantity, float temperature) {
+		super();
+		this.density = density;
+		this.sweetLevel = sweetLevel;
+		this.quantity = quantity;
+		this.temperature = temperature;
+	}
+	public String getDensity() {
+		return density;
+	}
+	public void setDensity(String density) {
+		this.density = density;
+	}
+	public int getSweetLevel() {
+		return sweetLevel;
+	}
+	public void setSweetLevel(int sweetLevel) {
+		this.sweetLevel = sweetLevel;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public float getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+	@Override
+	public String toString() {
+		return "Lassi [density=" + density + ", sweetLevel=" + sweetLevel + ", quantity=" + quantity + ", temperature="
+				+ temperature + "]";
+	}
+
+	
+	
+	
+}
+
+
+
+
+
+
+
+

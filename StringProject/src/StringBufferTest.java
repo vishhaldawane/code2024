c0dd5a31fbbca401f59bb42edcc6799b95a7c614
vishhaldawane <vishalviniing@gmail.com>
2024-10-10 16:47:38 +0530

    StringBuilderBufferPerformance

--- StringProject/src/StringBufferTest.java
+++ StringProject/src/StringBufferTest.java
@@ -7,15 +7,48 @@
   	|										|
   	only one thread is allowed		multiple threads can work
   	to work with it's method		with its method
-  
-  
+  	|										|
+  	two threads can't call 			two threads can call
+  	methods of StringBuffer			methods of StringBuilder
+  	|									|
+  	synchronized					not-synchronized
+  	|									|
+  	like : Tube Well				like : Open Well
+  	|									|
+  	less efficient					efficient
+  	
+  	StringBuffer buff;				StringBuilder build;
+  	|												|	thread3
+  	buff <-- thread1, thread2, thread3					|
+ 														|
+  											thread1 ->build <--thread2
+ 
+ 	StringBuilder
+ 				| at a time all 3 can shop
+ 				|
+ Brother -- ShoppingCart<-- You<-addItem1,
+  |			|			addItem3
+addItem7 	Sister		addItem5
+ 			|
+ 			addItem2
+ 			addItem4
+ 			addItem6
+ 			
+ 			
+ 			But at the Billing Counter1 <-- StringBuffer
+ 									|
+ 								ShoppingCart1
+ 								ShoppingCart2
+ 								ShoppingCard3
+ 						
+ 			
  */
 public class StringBufferTest {
 	//cant use multiple thread to work with StringBuffer
 	// mutable Strings
 	public static void main(String[] args) {
 	
-		StringBuffer buff = new StringBuffer("CG");
+	StringBuffer buff = new StringBuffer("CG");
 		System.out.println("buff : "+buff);
 		System.out.println("buff : "+buff.hashCode());
 		
@@ -27,7 +60,7 @@ public class StringBufferTest {
 		
 		System.out.println("----------");
 		
-		StringBuilder strBuild = new StringBuilder("CG");
+	StringBuilder strBuild = new StringBuilder("CG");
 		System.out.println("strBuild : "+strBuild);
 		System.out.println("strBuild : "+strBuild.hashCode());
 		

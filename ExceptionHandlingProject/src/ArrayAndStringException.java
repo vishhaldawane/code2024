576720865fde3fae8b68c42c2aa339b63f5b760e
vishhaldawane <vishalviniing@gmail.com>
2024-10-11 12:18:16 +0530

    StringExceptionTest2

--- ExceptionHandlingProject/src/ArrayAndStringException.java
+++ ExceptionHandlingProject/src/ArrayAndStringException.java
@@ -3,9 +3,12 @@ public class ArrayAndStringException {
 
 	public static void main(String[] args) {
 
-		int score[]= {10,20,30,40,50};
+		System.out.println("begin main....");
+		int score[]= null;// {10,20,30,40,50};
 		
+		//LOGIC1
 		try {
+			System.out.println("printing first value ...");
 			System.out.println("score 0 : "+score[0]);
 			System.out.println("score 1 : "+score[1]);
 			System.out.println("score 2 : "+score[20]);//<---
@@ -13,13 +16,18 @@ public class ArrayAndStringException {
 			System.out.println("score 4 : "+score[4]);
 		}
 		catch(ArrayIndexOutOfBoundsException ex) {
-			System.out.println("Invalid index for the array....");
+			System.out.println("1 Invalid index for the array....");
+		}
+		catch(NullPointerException ex) {
+			System.out.println("2 Problem  : "+ex);
 		}
 		
+		
+		//LOGIC2
 		//new activity atall
 		try
 		{
-			String str="Cap";
+			String str=null;
 			System.out.println("alphebet : "+str.charAt(0));
 			System.out.println("alphebet : "+str.charAt(1));
 			System.out.println("alphebet : "+str.charAt(2));
@@ -28,8 +36,13 @@ public class ArrayAndStringException {
 		}
 		catch(StringIndexOutOfBoundsException ex)
 		{
-			System.out.println("Problem 2 : "+ex);
+			System.out.println("3 Problem  : "+ex);
 		}
+		catch(NullPointerException ex) {
+			System.out.println("4 Problem : "+ex);
+		}
+		
+		System.out.println("END OF MAIN");
 	}
 
 }

e51bc1b00d21bef305a9c8574c9c3a2f54d85212
vishhaldawane <vishalviniing@gmail.com>
2024-10-11 14:43:57 +0530

    CustomException

--- ExceptionHandlingProject/src/CustomExceptionTest.java
+++ ExceptionHandlingProject/src/CustomExceptionTest.java
@@ -4,9 +4,21 @@ public class CustomExceptionTest {
 	public static void main(String[] args) {
 
 		System.out.println("begin main");
-		Car myCar = new Car("BMW");
-		myCar.drive();
 		
+		Car myCar = null;
+		
+		try
+		{
+			myCar = new Car("BMW"); //this line is susceptible to throw runtime exception
+			myCar.drive(); ////this line is susceptible to throw runtime exception
+		}
+		catch(Exception e) {
+			System.out.println("Problem1  : "+e);
+		}
+		
+		if(myCar == null) {
+			System.out.println("Alternate travel arrangement is done....");
+		}
 		System.out.println("End main");
 
 	}
@@ -18,21 +30,23 @@ class Car
 	String modelName;
 	boolean carKeyFound=false;
 	
-	Car(String m) {
+					//throws is compulsory since it is Checked
+	Car(String m) throws Exception
+	{
 		modelName = m;
 		
 		double val = Math.random()%10;
 		
-		if(val>0.95) { //sharp memory | u know where the keys are
+		if(val>0.35) { //sharp memory | u know where the keys are
 			carKeyFound=true;
 		}
-		else if(val>0.10) { //weak memory | forgotten where the key is?
+		else if(val<0.10) { //weak memory | forgotten where the key is?
 			carKeyFound=false;
 		}
 
 		
 		if(carKeyFound==false) {
-			RuntimeException ex = new RuntimeException("Car key not found....");
+			Exception ex = new Exception("Car key not found....");
 			throw ex; // exiting from this constructor....
 			/// constructor call is incomplete, object wont be
 			// created....
@@ -42,11 +56,17 @@ class Car
 		}
 	} //end of the constructor means completion of Car is successfull
 	
-	void drive()
+
+	void drive() //no need of throws clause - since it is unchecked
 	{
 		System.out.println("STARTED THE CAR....");
-		for(int i=1;i<=15;i++) {
+		for(int i=1;i<=50;i++) {
 			System.out.println("Driving..."+modelName+" Car "+i);
+			double val = Math.random()%10;
+			if(val >= 0.98 ) {
+				RuntimeException ex = new RuntimeException("Speed limit exceeded.......");
+				throw ex;
+			}
 		}
 	}
 }

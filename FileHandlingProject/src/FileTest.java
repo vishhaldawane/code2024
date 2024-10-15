f02c3e0fcc9989cf8214bab5cb9dcdd3c4e18974
vishhaldawane <vishalviniing@gmail.com>
2024-10-15 17:07:09 +0530

    Filehandling2

--- FileHandlingProject/src/FileTest.java
+++ FileHandlingProject/src/FileTest.java
@@ -5,19 +5,39 @@ import java.io.IOException;
 public class FileTest {
 	public static void main(String[] args) {
 		
+		int wordCount=0;
+		int lineCount=0;
+		int letterCount=0;
 		
 		try {
 			System.out.println("Trying to open a file...");
-			FileInputStream fin = new FileInputStream("/Users/apple/CG2024/Day1.txt");
+			FileInputStream fin = new FileInputStream("prince.txt");
 			System.out.println("File is open...\n");
 			
 			byte letter=0;
 			letter = (byte) fin.read(); //read the first letter, ie H of Hello
 			
 			while(letter!=-1) { // -1 to check the EOF
+				++letterCount;
+				
+				char ch=(char)letter;
+				if(ch=='\n') {
+					++lineCount;
+					
+					System.out.println("\n-------------");
+				}
+				
+				if(ch==' ' ||ch=='\n') {
+					++wordCount;
+				}
 				System.out.print((char)letter);
+			
 				letter = (byte) fin.read(); //continue reading from 2nd letter onwards
 			}
+			System.out.println("Lines "+lineCount);
+			System.out.println("Words "+wordCount);
+			System.out.println("Chars "+letterCount);
+
 			System.out.println("\nTrying to close the file...");
 			fin.close();
 			System.out.println("\nFile is closed...");
